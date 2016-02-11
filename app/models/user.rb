8010f854bc732edc987b09dcab9c7fb976449417
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-02-11 20:47:40 +0000

    changed validation to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,13 @@ class User
 
   property :id,       Serial, required: true
   property :name,     String, required: true
-  property :email,    String, required: true
+  property :email,    String, format: :email_address, required: true
   property :password_digest, BCryptHash
 
   attr_accessor :password_confirmation
   attr_reader :password
 
   validates_confirmation_of :password, :confirm => :password_confirmation
-
-  validates_format_of :email, :as => :email_address
-
 
   def password=(password)
     @password=password
